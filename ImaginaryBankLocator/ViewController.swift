3658cb0e67f5cdde59a2fd942afb7f4785c5a1c8
Majlinda Llugiqi <majlindallugiqi@majlindas-macbook-pro.local>
2019-02-16 13:20:55 +0100

    Initial Commit

---
+++ ImaginaryBankLocator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImaginaryBankLocator
+//
+//  Created by Majlinda Llugiqi on 2/16/19.
+//  Copyright © 2019 Majlinda Llugiqi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
